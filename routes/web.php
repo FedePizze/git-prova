dc6fff669a4dcebfb6c8d9f8b88976e3f9a0d3fe
antoniogiachin <giachinantonio@outlook.com>
2022-04-20 15:14:00 +0200

    progetto inizializzato (auth Vue, Bootstrap 5, VueRouter)

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+// rotta catch all
+Route::get("{any?}",
+    function () {
+        return view('guests.home');
+    }
+)->where('any', '.*');
